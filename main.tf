9e0aaad245a466dc95ae2e62461c2f15c84532eb
sscarfone <steve.scarfone@gmail.com>
2022-12-14 13:58:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,15 +31,18 @@ module "vpc" {
   }
 }
 
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
-}
+}  
+  
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
