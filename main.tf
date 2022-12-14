d457af45c1acf3e28fbb1850814943dfd40190c7
sscarfone <steve.scarfone@gmail.com>
2022-12-14 15:50:06 -0500

    add missing [

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "autoscaling" {
   max_size = 2
   vpc_zone_identifier   = module.vpc.public_subnets
   target_group_arns     = blog-alb.target_group_arns
-  security_groups       = module.blog_sg.security_group_id]
+  security_groups       = [module.blog_sg.security_group_id]
 
   image_id              = data.aws_ami.app_ami.id
   instance_type         = var.instance_type
