b54b88ac8d5c9f3ebbd8360807f566ae349703ad
sscarfone <steve.scarfone@gmail.com>
2022-12-14 16:00:11 -0500

    partial copy from branch

--- main.tf
+++ main.tf
@@ -37,14 +37,15 @@ module "autoscaling" {
   version = "6.5.3"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
-  vpc_zone_identifier   = module.vpc.public_subnets
-  target_group_arns     = blog-alb.target_group_arns
-  security_groups       = [module.blog_sg.security_group_id]
 
-  image_id              = data.aws_ami.app_ami.id
-  instance_type         = var.instance_type
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
 
 }
 
