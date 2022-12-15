9595bfb776ae1f8c4c09557b820e0df60ae92149
sscarfone <steve.scarfone@gmail.com>
2022-12-15 12:53:11 -0500

    copy from answers once agaim

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner]
+  owners = [var.ami_filter.owner] # Bitnami
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -36,10 +37,10 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "log"
+  name = "blog"
 
-  min_size            = var.asg_min_size
-  max_size            = var.asg_max_size
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
@@ -51,7 +52,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "${var.environment.name}-blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
@@ -61,7 +62,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-blog-"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -86,7 +87,7 @@ module "blog_sg" {
   version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
-  name    = "${var.environment.name}-blog"
+  name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
--- variables.tf
+++ variables.tf
@@ -33,12 +33,12 @@ default = {
 }
 
 
-variable "asg_min_size" {
+variable "asg_min" {
   description = "Minimum number of instances in the ASG"
   default = 1
 }
 
-variable "asg_max_size" {
+variable "asg_max" {
   description = "Maximum number of instances in the ASG"
   default = 2
 }
